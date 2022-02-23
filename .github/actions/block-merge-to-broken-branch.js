5fc3581ba369efe926ab9c18dbe1fd5efe9a0f63
sobadgirl <87839912+sobadgirl@users.noreply.github.com>
2022-02-23 12:02:54 +0800

    feat: add block merge action script

---
+++ .github/actions/block-merge-to-broken-branch.js
@@ -0,0 +1,47 @@
+try {
+    const { Octokit } = require("@octokit/action");
+    const core = require('@actions/core');
+  
+    const octokit = new Octokit();
+    const event = process.env.GITHUB_EVENT_NAME
+    const eventPayload = require(process.env.GITHUB_EVENT_PATH)
+  
+    const owner = "UrbanCompass"
+    const repo = "glide-devapp"
+  
+    // 1. 获取当前分支 和 author
+    // 2. 获取 target 分支的状态
+    // 3. 获取 broken 的作者
+    // 4. 判断 broken 是否由自己造成
+    // 5. block merge
+  
+  
+    if (["main", "master", "development"].indexOf(eventPayload.base.ref) === -1) {
+      process.exit(0)
+    }
+  
+    const checkRuns = await octokit.request('GET /repos/{owner}/{repo}/commits/{ref}/check-runs', {
+      owner,
+      repo,
+      ref: eventPayload.base.ref,
+      status: "completed",
+      // check_name: "build"
+    })
+  
+    if (checkRuns.length === 0){
+      process.exit(0)
+    }
+  
+    checkRuns.each(run => {
+      console.log("Run Info: ", run.id, run.name, ", ", run.conclusion)
+    })
+  
+    const checkRun = checkRuns[0]
+  
+    if (checkRun.conclusion === "failure") {
+      throw "Exists broken branch!"
+    }
+  
+  } catch (e) {
+    core.setFailed(e)
+  }
