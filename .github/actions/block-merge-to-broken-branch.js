da1310d97ffc8dd4c294217465919dfad91b15c2
sobadgirl <87839912+sobadgirl@users.noreply.github.com>
2022-02-23 17:44:27 +0800

    ff

--- .github/actions/block-merge-to-broken-branch.js
+++ .github/actions/block-merge-to-broken-branch.js
@@ -1,49 +1,47 @@
 async function main() {
-    const github = require('@actions/github');
-    const core = require('@actions/core');
-  
-    const octokit = github.getOctokit(process.env.GITHUB_TOKEN)
+  const github = require('@actions/github');
 
-    const event = process.env.GITHUB_EVENT_NAME
-    const eventPayload = require(process.env.GITHUB_EVENT_PATH)
-  
-    const owner = "MotherFuckerChan"
-    const repo = "test"
-  
-    // 1. 获取当前分支 和 author
-    // 2. 获取 target 分支的状态
-    // 3. 获取 broken 的作者
-    // 4. 判断 broken 是否由自己造成
-    // 5. block merge
-  
-    if (["main", "master", "development"].indexOf(eventPayload.pull_request.base.ref) === -1) {
-      process.exit(0)
-    }
-  
-    const checkRuns = await octokit.rest.checks.listForRef({
-      owner,
-      repo,
-      ref: eventPayload.pull_request.base.ref,
-      status: "completed",
-      // check_name: "build"
-    })
+  const octokit = github.getOctokit(process.env.GITHUB_TOKEN)
 
-    console.log(checkRuns)
-  
-    if (checkRuns.length === 0){
-      process.exit(0)
-    }
-  
-    checkRuns.each(run => {
-      console.log("Run Info: ", run.id, run.name, ", ", run.conclusion)
-    })
-  
-    const checkRun = checkRuns[0]
-  
-    if (checkRun.conclusion === "failure") {
-      throw "Exists broken branch!"
-    }
-  
+  const event = process.env.GITHUB_EVENT_NAME
+  const eventPayload = require(process.env.GITHUB_EVENT_PATH)
+
+
+  const owner = eventPayload.repository.name
+  const repo = eventPayload.repository.owner.login
+
+  // 1. 获取当前分支 和 author
+  // 2. 获取 target 分支的状态
+  // 3. 获取 broken 的作者
+  // 4. 判断 broken 是否由自己造成
+  // 5. block merge
+
+  if (["main", "master", "development"].indexOf(eventPayload.pull_request.base.ref) === -1) {
+    process.exit(0)
   }
+
+  const { data: checkRunsInfo } = await octokit.rest.checks.listForRef({
+    owner,
+    repo,
+    ref: eventPayload.pull_request.base.ref,
+    status: "completed",
+    // check_name: "build"
+  })
+
+  if (checkRunsInfo.total_count === 0){
+    process.exit(0)
+  }
+
+  checkRunsInfo.check_runs.each(run => {
+    console.log("Run Info: ", run.id, run.name, ", ", run.conclusion)
+  })
+
+  const checkRun = checkRunsInfo.check_runs[0]
+
+  if (checkRun.conclusion === "failure") {
+    throw "Exists broken branch!"
+  }
+
+}
 
 main()
