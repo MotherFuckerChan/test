42ff29be546558a735eb82489491e0cfd9e4a53b
zhibing.chen <drcbin@gmail.com>
2022-02-25 17:07:34 +0800

    FFF

--- .github/actions/block-merge-to-broken-branch.js
+++ .github/actions/block-merge-to-broken-branch.js
@@ -1,60 +1,47 @@
 async function main() {
   const github = require('@actions/github');
+  const core = require('@actions/core');
 
   const octokit = github.getOctokit(process.env.GITHUB_TOKEN)
 
-  const event = process.env.GITHUB_EVENT_NAME
   const eventPayload = require(process.env.GITHUB_EVENT_PATH)
-
 
   const repo = eventPayload.repository.name
   const owner = eventPayload.repository.owner.login
   const baseRef = eventPayload.pull_request.base.ref
 
-  console.log({owner, repo, baseRef})
 
-  // 1. 获取当前分支 和 author
-  // 2. 获取 target 分支的状态
-  // 3. 获取 broken 的作者
-  // 4. 判断 broken 是否由自己造成
-  // 5. block merge
-
+  // TODO(zhibing.chen) branches as a input param.
   if (["main", "master", "development"].indexOf(eventPayload.pull_request.base.ref) === -1) {
     process.exit(0)
   }
 
-  const { data: deployments } = await octokit.rest.repos.listDeployments({
+  const { data: states} = await octokit.rest.repos.listCommitStatusesForRef({
     owner,
     repo,
-    ref: baseRef,
-    // status: "completed",
-    // check_name: "build"
+    ref: baseRef
   })
 
-  console.log("Deployments: ", deployments)
-
-  if (deployments.length === 0){
+  if (states.length === 0){
     process.exit(0)
   }
 
-  deployments.forEach(async deploy => {
-    console.log("Deploy Id: ", deploy.id, "==============")
-    const { data: states }= await octokit.rest.repos.listDeploymentStatuses({
-      owner,
-      repo,
-      deployment_id: deploy.id
-    })
-    states.forEach(st => {
-      console.log(st.state, " => ", st.target_url)
-    })
-  })
+  const state = states[0]
 
-  const checkRun = checkRunsInfo.check_runs[0]
+  const { data: {author: committer}} = await octokit.rest.repos.getCommit({
+    owner,
+    repo,
+    ref: baseRef,
+  });
 
-  if (checkRun.conclusion === "failure") {
-    throw "Exists broken branch!"
+  // never block the user who made the branch broken 
+  if (state.state !== "success" && eventPayload.pull_request.user.login !== committer.login) {
+    core.setFailed(`PR was blocked because target branch ${baseRef} was broken/pending(not success). see broken detail ${state.target_url}`)
   }
-
 }
 
-main()
+try {
+  main()
+} catch (e){
+  core.setFailed(e)
+}
---
+++ .github/actions/update-broken-block-status.js
@@ -0,0 +1,73 @@
+async function main() {
+  const github = require('@actions/github');
+
+  const octokit = github.getOctokit(process.env.GITHUB_TOKEN)
+  const eventPayload = require(process.env.GITHUB_EVENT_PATH)
+
+  const repo = eventPayload.repository.name
+  const owner = eventPayload.repository.owner.login
+
+  const commitState = eventPayload.state === "success" ? "success" : "failure"
+
+  let branch2commit = {}
+  let branches = (await Promise.all(["master", "development", "staging-infra-china"].map(async branch => {
+    const { data: { commit }} =  await octokit.rest.repos.getBranch({
+      owner,
+      repo,
+      branch: branch
+    });
+    branch2commit[branch] = commit
+    return commit.sha === eventPayload.sha ? branch : null
+  }))).filter(Boolean)
+
+  let prs = []
+  for (const branch of branches) {
+    for (let page = 1; ; page++ ) {
+      const { data: pagedData } = await octokit.rest.pulls.list({
+        owner,
+        repo,
+        base: branch,
+        state: "open",
+        per_page: 100,
+        page
+      })
+      if (pagedData.length === 0) { break }
+      prs = prs.concat(pagedData)
+    }
+  }
+
+  const checkRuns = (await Promise.all(prs.map(async pr => {
+    const { data: { check_runs } } = await octokit.rest.checks.listForRef({
+      owner,
+      repo,
+      ref: pr.head.sha
+    })
+    const filteredRuns = check_runs
+      .filter(run => run.name === "branch-broken-check")
+      .filter(() => (commitState === "success") || (commitState === "failure" && pr.user.login !== branch2commit[pr.base.ref].author.login))
+      .slice(0, 1)
+    return filteredRuns
+  }))).flat()
+
+  checkRuns.forEach(async run => {
+    await octokit.rest.checks.update({
+      owner, 
+      repo, 
+      check_run_id: run.id,
+      conclusion: commitState,
+      output: {
+        title: `Blocking`,
+        summary: `Merge are blocked because branch ${branches} were broken.`,
+        text: `broken commit(s): \n ` + branches.map(branch => {
+          return `- [${branch}](${branch2commit[branch].html_url}): [${eventPayload.target_url}](${eventPayload.target_url})`
+        }).join("\n")
+      }
+    })
+  })
+}
+
+try {
+  main()
+} catch (e){
+  core.setFailed(e)
+}
